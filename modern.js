f03e22fad3e288c9745ee6a19478710c1ecaf9d3
Søren Birkemeyer <sb@webfactory.de>
2023-11-13 19:50:53 +0000

    Add browserslist config for modern browsers with mininum baseline for 2023

---
+++ modern.js
@@ -0,0 +1,11 @@
+module.exports = [
+  'defaults',
+  'last 3 Android major versions',
+  'last 3 iOS major versions',
+  'ff >= 102',
+  'chrome >= 108',
+  'edge >= 108',
+  'safari >= 13',
+  'opera >= 62',
+  'not dead',
+];
