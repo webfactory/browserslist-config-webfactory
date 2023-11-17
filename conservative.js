0fc6367d819c3ac6e9acf8d07988b0c94a4857a7
Søren Birkemeyer <sb@webfactory.de>
2023-11-17 13:07:06 +0100

    Update browserslist config with mininum baseline anno 2023 (#2)

---
+++ conservative.js
@@ -0,0 +1,11 @@
+module.exports = [
+  '>0.25% in DE',
+  'last 3 Chrome major versions',
+  'last 3 Firefox major versions',
+  'last 3 Edge major versions',
+  'last 3 Safari major versions',
+  'ie 11',
+  'last 3 Android major versions',
+  'last 3 ChromeAndroid major versions',
+  'last 3 iOS major versions'
+];
