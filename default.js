8c1a6ed32049a97f5d86c021fbf68f98251ce586
Søren Birkemeyer <sb@webfactory.de>
2023-11-17 12:04:38 +0000

    Update aggressively: default is now leaner, old config still available via conservative

--- default.js
+++ default.js
@@ -1,11 +1,12 @@
 module.exports = [
-  '>0.25% in DE',
-  'last 3 Chrome major versions',
-  'last 3 Firefox major versions',
-  'last 3 Edge major versions',
-  'last 3 Safari major versions',
-  'ie 11',
+  '>=0.25% in DE',
   'last 3 Android major versions',
-  'last 3 ChromeAndroid major versions',
-  'last 3 iOS major versions'
+  'last 3 iOS major versions',
+  'ff >= 102',
+  'chrome >= 108',
+  'edge >= 108',
+  'safari >= 13',
+  'opera >= 62',
+  'Firefox ESR',
+  'not dead',
 ];
